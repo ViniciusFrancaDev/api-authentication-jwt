cac809b4a84830208d8cfd45f7430097aeaa4343
Vinicius Franca <vfranca14@gmail.com>
2022-02-18 14:36:41 -0300

    Refactoring project structure and creating register endpoint

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
