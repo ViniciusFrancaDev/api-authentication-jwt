cc74d0f583d0d6d361831b122cc53b8200849ebd
Vinicius Franca <vfranca14@gmail.com>
2022-03-02 18:06:53 -0300

    Adjusting code patterns using black

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import RegisterView, LoginView, UserView, LogoutView, RefreshView
 
 urlpatterns = [
-    path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view()),
-    path('user', UserView.as_view()),
-    path('logout', LogoutView.as_view()),
-    path('refresh', RefreshView.as_view()),
+    path("register", RegisterView.as_view()),
+    path("login", LoginView.as_view()),
+    path("user", UserView.as_view()),
+    path("logout", LogoutView.as_view()),
+    path("refresh", RefreshView.as_view()),
 ]
